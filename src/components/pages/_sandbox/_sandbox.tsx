46645e3476aeb22f0573ddde39bbab9e6a9dfcae
Ali <deneb.25@ya.ru>
2021-01-16 21:27:30 +0100

    Overview restored

--- src/components/pages/_sandbox/_sandbox.tsx
+++ src/components/pages/_sandbox/_sandbox.tsx
@@ -1,23 +1,32 @@
 import './_sandbox.scss';
 import React from 'react';
-// import NavBar from '../../navigation/navbar';
-// import Breadcrumb from '../../navigation/breadcrumb';
-// import Footer from '../../footer';
+import NavBar from '../../navigation/navbar';
+import Breadcrumb from '../../navigation/breadcrumb';
+import Footer from '../../footer';
 import { COLORS } from '../../../config';
+import { Overview } from '../overview';
+
+const Page = () => (
+  <div className="d-flex">
+    <div className="col-4 me-3 rec" />
+    <div className="col-3 me-3 rec" />
+    <div className="col-3 me-3 rec" />
+  </div>
+);
 
 const Sandbox = () => {
   return (
-    <div style={{ width: '100vw', height: '100vh', padding: '100px 100px', backgroundColor: COLORS.bg }}>
-      <div>S</div>
-    </div>
-    // <>
-    //   <NavBar />
-    //   <main className="content-area">
-    //     <Breadcrumb />
-    //     <Study />
-    //     <Footer className="footer" />
-    //   </main>
-    // </>
+    // <div style={{ width: '100vw', height: '100vh', padding: '100px 100px', backgroundColor: COLORS.tertiary }}>
+    //   <Page />
+    // </div>
+    <>
+      <NavBar />
+      <main className="content-area">
+        <Breadcrumb />
+        <Overview />
+        <Footer className="footer" />
+      </main>
+    </>
   );
 };
 
--- src/components/pages/overview.tsx
+++ src/components/pages/overview.tsx
@@ -11,17 +11,17 @@ import LastEdited from '../overview/last-edited';
 export const Overview = () => {
   const info = useInfo();
   return (
-    <div className="statistics container-fluid">
-      <div className="row mb-3">
+    <div className="statistics">
+      <div className="d-flex mb-3">
         <LastTrainings lastTrainings={info.overview.lastTrainings} />
         <CardsPerDay data={info.overview.cardsPerDayData} />
       </div>
-      <div className="row mb-3">
+      <div className="d-flex mb-3">
         <DecksProgress deckProgresses={info.overview.deckProgresses} />
         <CardsTypes data={info.overview.cardsTypesData} />
         <LastEdited lastEditedCards={info.overview.lastEditedCards} />
       </div>
-      <div className="row">
+      <div className="d-flex">
         <TimeInApp data={info.overview.timeInAppData} />
         <LastReviews lastReviews={info.overview.lastReviews} />
       </div>
