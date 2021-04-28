5652ca8e4e775c0b96eec8d22b36c78ea794c479
Ali <deneb.25@ya.ru>
2021-04-28 12:35:10 +0200

    problems handled

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,12 @@ import './components/icons/icons.scss';
 import './components/images/images.scss';
 
 import InfoProvider from './components/context/info-provider';
-import { _ROOT, _SANDBOX, OVERVIEW, PAGES } from './components/pages';
+import { _ROOT, _SANDBOX, PAGES, STUDY } from './components/pages';
 import { buildRoutes } from './components/utils/routing';
 import { PagesInfoProvider } from './components/context/user-position-provider';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-let redirect = OVERVIEW;
+let redirect = STUDY;
 
 if (process.env.NODE_ENV === 'development') {
   redirect = _SANDBOX;
